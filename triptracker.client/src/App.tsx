ef2072e909e0612f1bd3dcf2c3ebd0627265530b
Ruben Baader <rubenbaader@gmail.com>
2024-06-28 16:32:58 +0200

    implemented file structure for frontend layout, routing

--- triptracker.client/src/App.tsx
+++ triptracker.client/src/App.tsx
@@ -1,12 +1,23 @@
 import './App.css'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 import { TripForm } from './components/TripForm'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <div>Layout</div> }>
+          <Route index element={ <div>Home</div> } />
+          <Route path='login' element={ <div>Login</div> } />
+          <Route path='*' element={ <div>404</div> } />
+        </Route>
+      </Routes>
+
       <h1>Hi mom</h1>
       <TripForm />
-    </>
+    </BrowserRouter>
   )
 }
 
