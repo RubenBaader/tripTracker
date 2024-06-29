4e6ec0538c0bc79f7788862fa1c7f42bb60b539d
Ruben Baader <rubenbaader@gmail.com>
2024-06-29 10:43:32 +0200

    added bare-bones functionality to pages and routing

--- triptracker.client/src/App.tsx
+++ triptracker.client/src/App.tsx
@@ -1,22 +1,21 @@
 import './App.css'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-
-import { TripForm } from './components/TripForm'
+import Layout from './pages/Layout'
+import ErrorPage from './pages/Error.Page'
+import Home from './pages/Home.Page'
+import Login from './pages/Login.Page'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={ <div>Layout</div> }>
-          <Route index element={ <div>Home</div> } />
-          <Route path='login' element={ <div>Login</div> } />
-          <Route path='*' element={ <div>404</div> } />
+        <Route path='/' element={<Layout />}>
+          <Route index element={ <Home /> } />
+          <Route path='login' element={ <Login /> } />
+          <Route path='*' element={ <ErrorPage /> } />
         </Route>
       </Routes>
-
-      <h1>Hi mom</h1>
-      <TripForm />
     </BrowserRouter>
   )
 }
---
+++ triptracker.client/src/pages/Home.Page.tsx
@@ -0,0 +1,13 @@
+import { TripForm } from "../components/TripForm"
+
+
+const Home = () => {
+    return(
+        <>
+            <h1>Hi mom</h1>
+            <TripForm />
+        </>
+    )
+}
+
+export default Home;
